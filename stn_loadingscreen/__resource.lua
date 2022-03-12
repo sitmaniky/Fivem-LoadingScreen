44b6d5a42963c9e982fc7328efac24b4ec9ce936
ToKyoV2 <88289890+ToKyoV2@users.noreply.github.com>
2022-03-12 22:42:20 +0100

    Add files via upload

---
+++ stn_loadingscreen/__resource.lua
@@ -0,0 +1,23 @@
+
+
+description 'loadingscreen'
+
+
+
+files {
+
+    -- Main
+
+    'edit_this.html',
+
+    'keks.css',
+
+    'music/song.mp3',
+    
+    'background.jpg'
+
+}
+
+
+
+loadscreen 'edit_this.html'
